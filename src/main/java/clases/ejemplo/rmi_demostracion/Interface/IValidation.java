406a2045fa9b739d0f1987d819bdbd1ab98eba8e
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-06-08 00:01:23 -0400

    remove unused generic interface

--- src/main/java/clases/ejemplo/rmi_demostracion/Interface/IValidation.java
+++
@@ -1,7 +0,0 @@
-package clases.ejemplo.rmi_demostracion.Interface;
-
-import java.rmi.Remote;
-
-public interface IValidation<T> extends Remote {
-    boolean consultData(T input) throws Exception;
-}
