b3d5e5e1cde513eadb4a52580f0d678452c42d23
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-06-08 00:15:35 -0400

    remove hardcoded port in muni

--- src/main/java/clases/ejemplo/rmi_demostracion/Server/MuniServer.java
+++ src/main/java/clases/ejemplo/rmi_demostracion/Server/MuniServer.java
@@ -14,6 +14,7 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class MuniServer extends UnicastRemoteObject implements PreguntarPermiso {
     private static SessionFactory sessionFactory;
+    private static final int PORT = 1101;
 
     public MuniServer() throws RemoteException {
         super();
@@ -48,7 +49,7 @@ public class MuniServer extends UnicastRemoteObject implements PreguntarPermiso 
 
     public static void main(String[] args) {
         try {
-            LocateRegistry.createRegistry(1101);
+            LocateRegistry.createRegistry(PORT);
             MuniServer obj = new MuniServer();
             Naming.rebind("//localhost/PreguntarPermiso", obj);
         } catch (Exception e) {
